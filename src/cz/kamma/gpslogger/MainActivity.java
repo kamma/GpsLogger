c2d73f6c0b8a9101af3fe801ccf34df77d8c0cc3
kAmMa <kamma.cz@gmail.com>
2018-10-25 11:11:42 +0200

    working

--- src/cz/kamma/gpslogger/MainActivity.java
+++ src/cz/kamma/gpslogger/MainActivity.java
@@ -27,269 +27,247 @@ import java.util.Date;
 
 public class MainActivity extends Activity implements LocationListener {
 
-    private static final String[] INITIAL_PERMS={
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE
-    };
+	private static final String[] INITIAL_PERMS = { Manifest.permission.ACCESS_FINE_LOCATION,
+			Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE,
+			Manifest.permission.READ_EXTERNAL_STORAGE };
 
-    private static final int LOCATION_REQUEST=1340;
+	private static final int LOCATION_REQUEST = 1340;
 
-    long start = -1;
-    boolean running = false;
+	long start = -1;
+	boolean running = false;
 
-    static final String TAG = MainActivity.class.getCanonicalName();
+	static final String TAG = MainActivity.class.getCanonicalName();
 
-    Button buttonStart, buttonStop, buttonReplay, buttonReplayStop;
-    LocationManager locationManager;
-    FileOutputStream f;
-    TextView textView;
+	Button buttonStart, buttonStop, buttonReplay, buttonReplayStop;
+	LocationManager locationManager;
+	FileOutputStream f;
+	TextView textView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 
-        buttonStart = findViewById(R.id.buttonStart);
-        buttonStop = findViewById(R.id.buttonStop);
-        buttonStop.setEnabled(false);
-        buttonReplay = findViewById(R.id.startReplay);
-        buttonReplayStop = findViewById(R.id.stopReplay);
-        buttonReplayStop.setEnabled(false);
-        textView = findViewById(R.id.textView);
+		buttonStart = findViewById(R.id.buttonStart);
+		buttonStop = findViewById(R.id.buttonStop);
+		buttonStop.setEnabled(false);
+		buttonReplay = findViewById(R.id.startReplay);
+		buttonReplayStop = findViewById(R.id.stopReplay);
+		buttonReplayStop.setEnabled(false);
+		textView = findViewById(R.id.textView);
 
-        buttonStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(TAG, "Start logging");
-                buttonStop.setEnabled(true);
-                buttonStart.setEnabled(false);
-                buttonReplay.setEnabled(false);
-                buttonReplayStop.setEnabled(false);
-                textView.setText("");
-                startLogging();
-            }
-        });
+		buttonStart.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Log.i(TAG, "Start logging");
+				buttonStop.setEnabled(true);
+				buttonStart.setEnabled(false);
+				buttonReplay.setEnabled(false);
+				buttonReplayStop.setEnabled(false);
+				textView.setText("");
+				startLogging();
+			}
+		});
 
-        buttonStop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(TAG, "Stop logging");
-                buttonStop.setEnabled(false);
-                buttonStart.setEnabled(true);
-                buttonReplay.setEnabled(true);
-                buttonReplayStop.setEnabled(false);
-                stopLogging();
-            }
-        });
+		buttonStop.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Log.i(TAG, "Stop logging");
+				buttonStop.setEnabled(false);
+				buttonStart.setEnabled(true);
+				buttonReplay.setEnabled(true);
+				buttonReplayStop.setEnabled(false);
+				stopLogging();
+			}
+		});
 
-        buttonReplay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(TAG, "Start replay");
-                buttonReplayStop.setEnabled(true);
-                buttonReplay.setEnabled(false);
-                buttonStop.setEnabled(false);
-                buttonStart.setEnabled(false);
-                startReplay();
-            }
-        });
+		buttonReplay.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Log.i(TAG, "Start replay");
+				running = true;
+				buttonReplayStop.setEnabled(true);
+				buttonReplay.setEnabled(false);
+				buttonStop.setEnabled(false);
+				buttonStart.setEnabled(false);
+				startReplay();
+			}
+		});
 
-        buttonReplayStop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(TAG, "Stop replay");
-                buttonReplayStop.setEnabled(false);
-                buttonReplay.setEnabled(true);
-                buttonStop.setEnabled(false);
-                buttonStart.setEnabled(true);
-                stopReplay();
-            }
-        });
-    }
+		buttonReplayStop.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Log.i(TAG, "Stop replay");
+				running = false;
+				buttonReplayStop.setEnabled(false);
+				buttonReplay.setEnabled(true);
+				buttonStop.setEnabled(false);
+				buttonStart.setEnabled(true);
+				stopReplay();
+			}
+		});
+	}
 
-    private void startReplay() {
-        if (!canAccessLocation()) {
-            requestPermissions(INITIAL_PERMS, LOCATION_REQUEST);
-        }
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+	private void startReplay() {
+		if (!canAccessLocation()) {
+			requestPermissions(INITIAL_PERMS, LOCATION_REQUEST);
+		}
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        File root = Environment.getExternalStorageDirectory();
+		locationManager.addTestProvider(LocationManager.GPS_PROVIDER, true, true, true, false, true, true, true, 3, 1);
+		locationManager.setTestProviderEnabled(LocationManager.GPS_PROVIDER, true);
 
-        running = true;
+		File root = Environment.getExternalStorageDirectory();
 
-        try {
-            File file = new File(root,"Download/gpsdata.txt");
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            String line = br.readLine();
-            while (line!=null && running==true){
-                String[] data = line.split(" ");
-                if (data.length==4) {
-                    long delay = Long.parseLong(data[0]);
-                    double latitude = Double.parseDouble(data[1]);
-                    double longitude = Double.parseDouble(data[2]);
-                    double altitude = Double.parseDouble(data[3]);
-                    Thread.sleep(1000);
-                    setMockLocation(latitude, longitude, altitude);
-                }
-                line = br.readLine();
-            }
-            br.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+		try {
+			File file = new File(root, "Download/gpsdata.txt");
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String line = br.readLine();
+			while (line != null && running == true) {
+				String[] data = line.split(" ");
+				if (data.length == 4) {
+					long delay = Long.parseLong(data[0]);
+					
+					//latitude 50.19085392
+					//longitude 15.05299389
+					double
+					
+					double latitude = Double.parseDouble(data[1]);
+					double longitude = Double.parseDouble(data[2]);
+					double altitude = Double.parseDouble(data[3]);
+					long sleepTime = (long) (Math.random() * 50);
+					Thread.sleep((Math.random()>0.5?sleepTime+1000:1000-sleepTime));
+					setMockLocation(latitude, longitude, altitude);
+				}
+				line = br.readLine();
+			}
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    private int setMockLocationSettings() {
-        int value = 1;
-        try {
-            value = Settings.Secure.getInt(getContentResolver(),
-                    Settings.Secure.ALLOW_MOCK_LOCATION);
-            Settings.Secure.putInt(getContentResolver(),
-                    Settings.Secure.ALLOW_MOCK_LOCATION, 1);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return value;
-    }
+	private void setMockLocation(double latitude, double longitude, double altitude) {
+		Location newLocation = new Location(LocationManager.GPS_PROVIDER);
 
-    private void restoreMockLocationSettings(int restore_value) {
-        try {
-            Settings.Secure.putInt(getContentResolver(),
-                    Settings.Secure.ALLOW_MOCK_LOCATION, restore_value);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+		newLocation.setLatitude(latitude);
+		newLocation.setLongitude(longitude);
+		newLocation.setAltitude(altitude);
+		long time = System.currentTimeMillis();
+		newLocation.setTime(time);
+		newLocation.setAccuracy(1);
+		Method locationJellyBeanFixMethod = null;
+		try {
+			locationJellyBeanFixMethod = Location.class.getMethod("makeComplete");
+		} catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		}
+		if (locationJellyBeanFixMethod != null) {
+			try {
+				locationJellyBeanFixMethod.invoke(newLocation);
+			} catch (IllegalAccessException e) {
 
-    private void setMockLocation(double latitude, double longitude, double altitude) {
-        Location newLocation = new Location(LocationManager.GPS_PROVIDER);
+			} catch (InvocationTargetException e) {
+				e.printStackTrace();
+			}
+		}
+		try {
+			Log.i(TAG, "Setting location...");
+			locationManager.setTestProviderLocation(LocationManager.GPS_PROVIDER, newLocation);
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		}
+	}
 
-        newLocation.setLatitude (latitude);
-        newLocation.setLongitude(longitude);
-        newLocation.setAltitude(altitude);
-        long time = System.currentTimeMillis();
-        newLocation.setTime(time);
-        newLocation.setAccuracy(1);
-        Method locationJellyBeanFixMethod = null;
-        try {
-            locationJellyBeanFixMethod =Location.class.getMethod("makeComplete");
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        }
-        if (locationJellyBeanFixMethod != null) {
-            try {
-                locationJellyBeanFixMethod.invoke(newLocation);
-            } catch (IllegalAccessException e) {
+	private void stopReplay() {
+		running = false;
+		locationManager.removeTestProvider(LocationManager.GPS_PROVIDER);
+	}
 
-            } catch (InvocationTargetException e) {
-                e.printStackTrace();
-            }
-        }
-        int value = setMockLocationSettings();//toggle ALLOW_MOCK_LOCATION on
-        try {
-            locationManager.addTestProvider(LocationManager.GPS_PROVIDER, true, true, true, false, true,
-                    true, true, 3, 1);
-            locationManager.setTestProviderEnabled(LocationManager.GPS_PROVIDER, true);
-            locationManager.setTestProviderLocation(LocationManager.GPS_PROVIDER, newLocation);
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        } finally {
-            restoreMockLocationSettings(value);//toggle ALLOW_MOCK_LOCATION off
-        }
-    }
+	private void stopLogging() {
+		locationManager.removeUpdates(this);
+		getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		try {
+			f.flush();
+			f.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    private void stopReplay() {
-        running = false;
-        locationManager.removeTestProvider(LocationManager.GPS_PROVIDER);
-    }
+	private void startLogging() {
+		if (!canAccessLocation()) {
+			requestPermissions(INITIAL_PERMS, LOCATION_REQUEST);
+		}
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		start = System.currentTimeMillis();
+		File root = Environment.getExternalStorageDirectory();
+		try {
+			f = new FileOutputStream(new File(root, "gpsdata" + (new Date()) + ".txt"), true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+	}
 
-    private void stopLogging() {
-        locationManager.removeUpdates(this);
-        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        try {
-            f.flush();
-            f.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	@Override
+	public void onLocationChanged(Location location) {
+		double latitude = location.getLatitude();
+		double longitude = location.getLongitude();
+		double altitude = location.getAltitude();
+		long time = System.currentTimeMillis() - start;
 
-    private void startLogging() {
-        if (!canAccessLocation()) {
-            requestPermissions(INITIAL_PERMS, LOCATION_REQUEST);
-        }
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        start = System.currentTimeMillis();
-        File root = Environment.getExternalStorageDirectory();
-        try {
-            f = new FileOutputStream(new File(root, "gpsdata"+(new Date())+".txt"), true);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-    }
+		logData(time, latitude, longitude, altitude);
+	}
 
-    @Override
-    public void onLocationChanged(Location location) {
-        double latitude = location.getLatitude();
-        double longitude = location.getLongitude();
-        double altitude = location.getAltitude();
-        long time = System.currentTimeMillis() - start;
+	private void logData(long time, double latitude, double longitude, double altitude) {
+		String data = time + " " + latitude + " " + longitude + " " + altitude + "\n";
+		Log.i(TAG, "time: " + time + " latitude: " + latitude + " longitude: " + longitude + " altitude: " + altitude);
+		textView.append(data);
+		try {
+			f.write(data.getBytes());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-        logData(time, latitude, longitude, altitude);
-    }
+	@Override
+	public void onProviderDisabled(String provider) {
+	}
 
-    private void logData(long time, double latitude, double longitude, double altitude) {
-        String data = time+" "+latitude+" "+longitude+" "+altitude+"\n";
-        Log.i(TAG, "time: "+time+" latitude: "+latitude+" longitude: "+longitude+" altitude: "+altitude);
-        textView.append(data);
-        try {
-            f.write(data.getBytes());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	@Override
+	public void onProviderEnabled(String provider) {
+	}
 
-    @Override
-    public void onProviderDisabled(String provider) {
-    }
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+	}
 
-    @Override
-    public void onProviderEnabled(String provider) {
-    }
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
 
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-    }
+		switch (requestCode) {
+		case LOCATION_REQUEST:
+			if (canAccessLocation()) {
+				Toast.makeText(this, "GPS permission allowed", Toast.LENGTH_LONG).show();
+			} else {
+				Toast.makeText(this, "Cannot log GPS", Toast.LENGTH_LONG).show();
+			}
+			break;
+		}
+	}
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+	private boolean canAccessLocation() {
+		for (String perm : INITIAL_PERMS) {
+			if (!hasPermission(perm))
+				return false;
+		}
+		return true;
+	}
 
-        switch(requestCode) {
-            case LOCATION_REQUEST:
-                if (canAccessLocation()) {
-                    Toast.makeText(this, "GPS permission allowed", Toast.LENGTH_LONG).show();
-                }
-                else {
-                    Toast.makeText(this, "Cannot log GPS", Toast.LENGTH_LONG).show();
-                }
-                break;
-        }
-    }
-
-    private boolean canAccessLocation() {
-        for (String perm:INITIAL_PERMS) {
-            if (!hasPermission(perm))
-                return false;
-        }
-        return true;
-    }
-
-    private boolean hasPermission(String perm) {
-        return(PackageManager.PERMISSION_GRANTED==checkSelfPermission(perm));
-    }
+	private boolean hasPermission(String perm) {
+		return (PackageManager.PERMISSION_GRANTED == checkSelfPermission(perm));
+	}
 
 }
